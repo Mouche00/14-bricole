d25708c169a34ec347b40821db15d512a414c726
Mushi <144794503+Mouche00@users.noreply.github.com>
2024-02-22 12:13:11 +0100

    add spatie package and fix login

--- app/Http/Controllers/SessionController.php
+++ app/Http/Controllers/SessionController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use Dotenv\Exception\ValidationException;
 use Illuminate\Http\Request;
 
 class SessionController extends Controller
@@ -17,11 +18,52 @@ class SessionController extends Controller
             'password'=> 'required',
             ]);
             
-        if(auth()->attempt($attributes)){
-          return redirect('/');
+        // if(auth()->attempt($attributes)){
+        //     if(auth()->user()->artisan()->first()->exists()) {
+
+        //     } elseif(auth()->user()->client()->first()->exists()) {
+                
+        //     } elseif(auth()->user()->admin()->first()->exists()) {
+
+        //     } else {
+
+        //         return back();
+        //     }
+        // }
+
+        if(! auth()->attempt($attributes)) {
+            throw ValidationException::withMessages([
+                'email' => 'Your provided credentials could not be verified.'
+            ]);
         }
 
-        return back();
+        session()->regenerate();
+
+        $redirect = 'login';
+        switch (true) {
+            case auth()->user()->hasRole('artisan'):
+                $redirect = 'artisan';
+                break;
+            case auth()->user()->hasRole('client'):
+                $redirect = 'client';
+                break;
+            case auth()->user()->hasRole('admin'):
+                $redirect = 'admin';
+                break;
+        }
+
+        // if(auth()->user()->hasRole('artisan')) {
+
+        //     return redirect()->route('artisan');
+        // } elseif (auth()->user()->hasRole('client')) {
+
+        //     return redirect()->route('client');
+        // } elseif (auth()->user()->hasRole('admin')) {
+
+        //     return redirect()->route('artisan');
+        // }
+
+        return redirect()->route($redirect);
     }
 
 
