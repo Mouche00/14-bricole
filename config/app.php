e02b637e2a1f1d1de7c6aa8e995f0b220e12caf7
chaabat <chaabat.02@gmail.com>
2024-02-22 12:29:57 +0100

    laravel/socialite

--- config/app.php
+++ config/app.php
@@ -182,7 +182,8 @@ return [
     */
 
     'aliases' => Facade::defaultAliases()->merge([
-        // 'Example' => App\Facades\Example::class,
+        'Socialite' => Laravel\Socialite\Facades\Socialite::class,
     ])->toArray(),
+    
 
 ];
--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,11 @@
 <?php
 
+use App\Http\Controllers\Auth\ProviderController;
 use App\Http\Controllers\Controller;
 use App\Http\Controllers\RegisterController;
 use Illuminate\Support\Facades\Route;
+
+
 
 /*
 |--------------------------------------------------------------------------
@@ -19,6 +22,14 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+//laravel social
+
+
+Route::get('/auth/{provider}/redirect', [ProviderController::class, 'redirect']);
+Route::get('/auth/{provider}/callback', [ProviderController::class, 'callback']);
+
+
+
 
 // Route::get('register', [Controller :: class, 'register']);
 Route::get('login', [Controller::class, 'login']);
@@ -26,4 +37,3 @@ Route::get('login', [Controller::class, 'login']);
 Route::get('signup', [RegisterController::class, 'index'])->name('signup');
 Route::post('client/signup', [RegisterController::class, 'client'])->name('signup.client');
 Route::post('artisan/signup', [RegisterController::class, 'artisan'])->name('signup.artisan');
-
