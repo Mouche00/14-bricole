88dd5e072a22e33a24984e11cec11b7cd31e14a4
walid-lhaila <walidlhaila00@gmail.com>
2024-02-26 11:49:26 +0100

    artisan domain Done

--- resources/views/artisan/artisanCompetances.blade.php
+++ resources/views/artisan/artisanCompetances.blade.php
@@ -122,37 +122,18 @@
                     </div>
             
                     <div id="dropdownContent" class="dropdown-content">
+                        @foreach($competances as $competance)
                         <label>
-                            <input type="checkbox" name="competances" value="1" class="mr-2" onchange="updateSelection(this)"> Option 1
+                            <input type="checkbox" name="competances" value="1" class="mr-2" onchange="updateSelection(this)">{{$competance->nom}}
                         </label>
-                        <label>
-                            <input type="checkbox" name="competances" value="2" class="mr-2" onchange="updateSelection(this)"> Option 2
-                        </label>
-                        <label>
-                            <input type="checkbox" name="competances" value="3" class="mr-2" onchange="updateSelection(this)"> Option 3
-                        </label>
-                        <label>
-                            <input type="checkbox" name="competances" value="4" class="mr-2" onchange="updateSelection(this)"> Option 4
-                        </label>
-                        <label>
-                            <input type="checkbox" name="competances" value="5" class="mr-2" onchange="updateSelection(this)"> Option 5
-                        </label>
+                        @endforeach
                     </div>
                 </div>
 
                 
             </div>
 
-            <div>
-                <label for="dropzone-file" class="flex items-center px-3 py-2  mx-auto mt-8 text-center bg-white border  rounded-lg cursor-pointer dark:border-gray-600 ">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6 text-gray-600 dark:text-gray-600" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-8l-4-4m0 0L8 8m4-4v12" />
-                    </svg>
-    
-                    <h2 class="mx-3 text-gray-600">Compétances Photo</h2>
-    
-                    <input id="dropzone-file" name="domain_photo" type="file" class="hidden" />
-                </label>           
+            <div>         
              </div>
         </div>
 
