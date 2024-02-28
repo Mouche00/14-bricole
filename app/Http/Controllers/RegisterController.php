0424f01f31c454f9fbbbb3bb462bb2c87fea2d09
chaabat <chaabat.02@gmail.com>
2024-02-28 14:55:11 +0100

    Update profile client V1

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -9,6 +9,7 @@ use \App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
+
 class RegisterController extends Controller
 {
 
@@ -100,4 +101,7 @@ class RegisterController extends Controller
 
         return redirect('/');
     }
+
+
+
 }
